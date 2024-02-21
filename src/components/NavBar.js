702554dd6ced4d1a61ba23bf7fd198ecde5252e0
Madeline Corman <madelinecorman@gmail.com>
2024-02-21 08:39:58 -0600

    Added hamburger menu for mobile view and started on responsive styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 const NavBar = () => {
   const [showTopOfPage, setShowTopOfPage] = useState(false);
+  const [openHamburger, setOpenHamburger] = useState(false);
+
   useEffect(() => {
     const handleShowTopOfPage = () => {
       window.pageYOffset > 500
@@ -13,14 +17,53 @@ const NavBar = () => {
       window.removeEventListener("scroll", handleShowTopOfPage);
     };
   }, []);
+
+  function handleShowHamburger() {
+    setOpenHamburger((prevState) => !prevState);
+  }
+
   return (
-    <div className="nav-bar">
-      <a href="#contact">Contact</a>
-      <a href="#blog">Blog</a>
-      <a href="#projects">Projects</a>
-      <a href="#about">About</a>
-      {showTopOfPage && <a href="#home">Home</a>}
-    </div>
+    <>
+      <div className="nav-bar">
+        <a href="#contact">Contact</a>
+        <a href="#blog">Blog</a>
+        <a href="#projects">Projects</a>
+        <a href="#about">About</a>
+        {showTopOfPage && <a href="#home">Home</a>}
+      </div>
+      <div className="hamburger">
+        <RxHamburgerMenu onClick={handleShowHamburger} />
+        {openHamburger && (
+          <ul>
+            <li>
+              <a href="#home" onClick={handleShowHamburger}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={handleShowHamburger}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#projects" onClick={handleShowHamburger}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#blog" onClick={handleShowHamburger}>
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={handleShowHamburger}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 

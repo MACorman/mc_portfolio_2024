4e7ddf85d9a95d887a29f417c319f4f82d44f81f
Madeline Corman <madelinecorman@gmail.com>
2024-05-30 08:45:08 -0500

    Fixed responsiveness of youtube imbed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,20 +9,80 @@ export default function Projects() {
   useEffect(() => {
     const updateWindowSize = () => {
       // maybe do something like if window less than XXX set size to [XXX, XXXX]
-      if (window.innerWidth < 1200) {
-        setSize(["400", "800"]);
+      if (window.innerWidth < 1400) {
+        setSize(["470", "900"]);
+      }
+      if (window.innerWidth < 1300) {
+        setSize(["450", "860"]);
+      }
+      if (window.innerWidth < 1230) {
+        setSize(["420", "800"]);
+      }
+      if (window.innerWidth < 1145) {
+        setSize(["400", "760"]);
+      }
+      if (window.innerWidth < 1090) {
+        setSize(["380", "720"]);
+      }
+      if (window.innerWidth < 1035) {
+        setSize(["360", "680"]);
       }
       if (window.innerWidth < 992) {
-        setSize(["300", "650"]);
+        setSize(["340", "640"]);
       }
-      if (window.innerWidth < 768) {
-        setSize(["200", "600"]);
+      if (window.innerWidth < 930) {
+        setSize(["320", "600"]);
+      }
+      if (window.innerWidth < 875) {
+        setSize(["300", "560"]);
+      }
+      if (window.innerWidth < 815) {
+        setSize(["280", "520"]);
+      }
+      if (window.innerWidth < 778) {
+        setSize(["260", "500"]);
+      }
+      if (window.innerWidth < 740) {
+        setSize(["230", "440"]);
+      }
+      if (window.innerWidth < 660) {
+        setSize(["210", "400"]);
       }
       if (window.innerWidth < 600) {
-        setSize(["200", "400"]);
+        setSize(["190", "360"]);
       }
-      if (window.innerWidth < 400) {
+      if (window.innerWidth < 550) {
+        setSize(["180", "320"]);
+      }
+      if (window.innerWidth < 500) {
+        setSize(["170", "300"]);
+      }
+      if (window.innerWidth < 480) {
+        setSize(["160", "280"]);
+      }
+      if (window.innerWidth < 450) {
+        setSize(["150", "260"]);
+      }
+      if (window.innerWidth < 430) {
+        setSize(["150", "250"]);
+      }
+      if (window.innerWidth < 410) {
         setSize(["150", "240"]);
+      }
+      if (window.innerWidth < 395) {
+        setSize(["140", "230"]);
+      }
+      if (window.innerWidth < 380) {
+        setSize(["140", "220"]);
+      }
+      if (window.innerWidth < 370) {
+        setSize(["130", "215"]);
+      }
+      if (window.innerWidth < 360) {
+        setSize(["130", "210"]);
+      }
+      if (window.innerWidth < 345) {
+        setSize(["130", "205"]);
       }
     };
 

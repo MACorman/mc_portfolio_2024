afe278b73505c233b21fa1de782bf8f00ac1a412
Madeline Corman <madelinecorman@gmail.com>
2024-06-04 09:14:49 -0500

    widened projects carousel

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,85 +4,85 @@ import YouTube from "react-youtube";
 import { useState, useEffect } from "react";
 
 export default function Projects() {
-  const [size, setSize] = useState(["600", "1000"]);
+  const [size, setSize] = useState(["600", "1020"]);
 
   useEffect(() => {
     const updateWindowSize = () => {
       // maybe do something like if window less than XXX set size to [XXX, XXXX]
       if (window.innerWidth < 1400) {
-        setSize(["470", "900"]);
+        setSize(["510", "920"]);
       }
       if (window.innerWidth < 1300) {
-        setSize(["450", "860"]);
+        setSize(["460", "870"]);
       }
       if (window.innerWidth < 1230) {
-        setSize(["420", "800"]);
+        setSize(["440", "810"]);
       }
       if (window.innerWidth < 1145) {
-        setSize(["400", "760"]);
+        setSize(["420", "780"]);
       }
       if (window.innerWidth < 1090) {
-        setSize(["380", "720"]);
+        setSize(["400", "740"]);
       }
       if (window.innerWidth < 1035) {
-        setSize(["360", "680"]);
+        setSize(["380", "700"]);
       }
       if (window.innerWidth < 992) {
-        setSize(["340", "640"]);
+        setSize(["360", "660"]);
       }
       if (window.innerWidth < 930) {
-        setSize(["320", "600"]);
+        setSize(["340", "620"]);
       }
       if (window.innerWidth < 875) {
-        setSize(["300", "560"]);
+        setSize(["320", "580"]);
       }
       if (window.innerWidth < 815) {
-        setSize(["280", "520"]);
+        setSize(["300", "540"]);
       }
       if (window.innerWidth < 778) {
-        setSize(["260", "500"]);
+        setSize(["280", "520"]);
       }
       if (window.innerWidth < 740) {
-        setSize(["230", "440"]);
+        setSize(["250", "455"]);
       }
       if (window.innerWidth < 660) {
-        setSize(["210", "400"]);
+        setSize(["230", "435"]);
       }
       if (window.innerWidth < 600) {
-        setSize(["190", "360"]);
+        setSize(["210", "395"]);
       }
       if (window.innerWidth < 550) {
-        setSize(["180", "320"]);
+        setSize(["200", "355"]);
       }
       if (window.innerWidth < 500) {
-        setSize(["170", "300"]);
+        setSize(["180", "335"]);
       }
       if (window.innerWidth < 480) {
-        setSize(["160", "280"]);
+        setSize(["170", "315"]);
       }
       if (window.innerWidth < 450) {
-        setSize(["150", "260"]);
+        setSize(["160", "295"]);
       }
       if (window.innerWidth < 430) {
-        setSize(["150", "250"]);
+        setSize(["160", "285"]);
       }
       if (window.innerWidth < 410) {
-        setSize(["150", "240"]);
+        setSize(["160", "275"]);
       }
       if (window.innerWidth < 395) {
-        setSize(["140", "230"]);
+        setSize(["150", "265"]);
       }
       if (window.innerWidth < 380) {
-        setSize(["140", "220"]);
+        setSize(["150", "255"]);
       }
       if (window.innerWidth < 370) {
-        setSize(["130", "215"]);
+        setSize(["140", "250"]);
       }
       if (window.innerWidth < 360) {
-        setSize(["130", "210"]);
+        setSize(["140", "245"]);
       }
       if (window.innerWidth < 345) {
-        setSize(["130", "205"]);
+        setSize(["140", "230"]);
       }
     };
 
